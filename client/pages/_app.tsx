5fcf6db9c0b3477efcfa4b580185bae22ed94b2d
Phillip Phan <colPhilPhan@gmail.com>
2024-05-26 10:58:59 -0500

    Seelection box

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,71 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { useEffect, useRef, useState } from "react";
+import {
+  Box,
+  useSelectionContainer
+} from "@air/react-drag-to-select";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = () => {
+  const [selectionBox, setSelectionBox] = useState<Box>();
+  const elementsContainerRef = useRef<HTMLDivElement | null>(null);
+  const selectableItems = useRef<Box[]>([]);
+
+  const { DragSelection } = useSelectionContainer({
+    onSelectionChange: (box) => {
+      /**
+       * Here we make sure to adjust the box's left and top with the scroll position of the window
+       * @see https://github.com/AirLabsTeam/react-drag-to-select/#scrolling
+       */
+      const scrollAwareBox: Box = {
+        ...box,
+        top: box.top + window.scrollY,
+        left: box.left + window.scrollX
+      };
+
+      setSelectionBox(scrollAwareBox);
+    },
+    onSelectionStart: () => {
+      console.log("OnSelectionStart");
+    },
+    onSelectionEnd: () => console.log("OnSelectionEnd"),
+    selectionProps: {
+      style: {
+        border: "2px dashed purple",
+        borderRadius: 4,
+        backgroundColor: "red",
+        opacity: 0.5
+      }
+    },
+    isEnabled: true
+  });
+
+  useEffect(() => {
+    if (elementsContainerRef.current) {
+      Array.from(elementsContainerRef.current.children).forEach((item) => {
+        const { left, top, width, height } = item.getBoundingClientRect();
+        selectableItems.current.push({
+          left,
+          top,
+          width,
+          height
+        });
+      });
+    }
+  }, []);
+
+  return (
+    <div className="container">
+      <DragSelection />
+      <div className="selection-box-info">
+        Selection Box:
+        <div>top: {selectionBox?.top || ""}</div>
+        <div>left: {selectionBox?.left || ""}</div>
+        <div>width: {selectionBox?.width || ""}</div>
+        <div>height: {selectionBox?.height || ""}</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
